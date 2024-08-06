4b64ecd52329921c230c9fdc744df72d480211da
liam <171575208+R1ammu@users.noreply.github.com>
2024-08-06 14:51:45 +0200

    idk honestly git is a mess

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
-// FIZZBUZZ
+let numbers = [23, 24, 24, 46, 35, 23, 23,23,85,58,31,82,84,22,72,16,35]
 
-let userInput = prompt("Procure the integer!");
-let userNumber = parseInt(userInput, 10);
 
-let b = "Buzz";
-let f = "Fizz";
-let fb = "FizzBuzz";
 
-function numbers(userNumber) {
-  if (isNaN(userNumber) || userNumber <= 0) {
-    console.log("The input is not a valid positive number.");
-    return;
-  }
 
-let int = 0;
 
-  while (userNumber > int) {
-    int++;
-    if (int % 3 === 0 && int % 5 === 0) {
-    console.log(fb);
-    } else if (int % 5 === 0) {
-      console.log(b)
-    } else if (int % 3 === 0) {
-      console.log(f)
-    } else {
-      console.log(int)
-    }
-  }
-}
 
-numbers(userNumber);
+
+
+
+
+
+console.log(numbers)
