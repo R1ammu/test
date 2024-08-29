576bd24933db9a5f9b5928b8c5859eb51c3ef845
Liam <171575208+R1ammu@users.noreply.github.com>
2024-08-29 04:29:59 +0200

    .

--- script.js
+++ script.js
@@ -1,40 +1,7 @@
-function reveresString(userInput) {
-    
-    let split = userInput.split('');
-    
-    split.reverse();
+const container = document.querySelector('#container');
 
-    let reversedStr = split.join('');
-
-    return reversedStr;
-
-    
-}
-
-// removeFromArray
-
-// const array = [1, 2, 3, 4]
-
-// function removeFromArray(input) {
-//     const newarr = array.push(input);
-
-//     return array;
-
-// }
+const content = document.createElement('div');
+content.classList.add("content");
 
 
-const array = [1, 2, 3, 4];
-
-function removeFromArray(input) {
-    // Add the input to the array
-    array.push(input);
-    
-    // Create a new array that is a copy of the modified array
-    const newarr = [...array]; // or Array.from(array)
-    
-    // Return the new array
-    return newarr;
-}
-
-console.log(removeFromArray('something'));
-
+container.appendChild(content);
